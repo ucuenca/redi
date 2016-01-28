6061f50979ff2b67300abd6d9282e7f33e734db9
fernandobac03 <fernando_bac03@hotmail.com>
2016-01-28 11:13:42 -0500

    Add multilingual support using Angularjs Translation. Add support for Spanish and English Upgrading the Main menu, now you have a unique menu template (mainmenu.html) this being injected in each view.  sanitize angular library added to get more security.

---
+++ webapp/src/main/webapp/wkhome/js/components/en.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function(){
+var myApp = angular.module("wkhomeApp");
+myApp.labels_en = {
+"version": "Version",
+"name": "Name",
+"surname": "Surname",
+"Select Research Area:":"Select Research Area:",
+"Areas de Investigacion":"Research Areas",
+"Total Autores":"Authors",
+"Total Publicaciones": "Publications",
+"home":"Home",
+"about":"About",
+"help":"Help",
+"contact":"Contact",
+};
+}());
