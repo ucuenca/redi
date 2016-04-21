a3136dc5feb500d1ed6ecd182de8724237ab71fd
fernandobac03 <fernando_bac03@hotmail.com>
2016-04-21 15:39:13 -0500

    Update sparql query in countResearchAreas.js Controllers Add subjets for each candidates authors in searchText.js Add Author information in explorableTree.js Optimizing sparql queries in different controllers and directives

--- webapp/src/main/webapp/wkhome/js/controllers/countPublications.js
+++ webapp/src/main/webapp/wkhome/js/controllers/countPublications.js
@@ -1,23 +1,35 @@
 
 wkhomeControllers.controller('countPublications', ['$scope', 'globalData', 'sparqlQuery',
     function ($scope, globalData, sparqlQuery) {
-        var queryTotalAuthors = globalData.PREFIX
-                + 'CONSTRUCT { '
-                + ' ?source a uc:Provider. '
-                + ' ?source uc:name ?name;  '
-                + '        uc:total ?total } '
-                + ' WHERE {  '
-                + '     SELECT ?source (substr(?source,8) as ?name) ?total '
-                + '         WHERE { '
-                + '             SELECT DISTINCT ?source (COUNT(?s) AS ?total) '
-                + '             WHERE { '
-                + '                 GRAPH <'+globalData.centralGraph+'> { '
-                + '                     ?s foaf:Organization  ?source. '
+        var queryTotalAuthors =   globalData.PREFIX
+                + ' CONSTRUCT '
+                + ' {  '
+                + '     ?prov uc:total ?totalp.'
+                + '     ?prov uc:name ?sname.'
+                + ' } '
+                + ' WHERE {'
+                + ' {'
+                + '     SELECT DISTINCT (SAMPLE(?provenance)  as ?prov) (SAMPLE(?sourcename)  as ?sname)  (count(DISTINCT ?pub) as ?totalp)'
+                + '         WHERE {'
+                + '             GRAPH <' + globalData.centralGraph + '> {'
+                + '                 ?s a foaf:Person.'
+                + '                 ?s foaf:name ?name.'
+                + '                 ?s foaf:publications ?pub.'
+                + '                 ?s dct:provenance ?provenance.     '
+                + '                 { '
+                + '                     SELECT * '
+                + '                     WHERE '
+                + '                     { '
+                + '                         graph <http://ucuenca.edu.ec/wkhuska/endpoints> '
+                + '                         { '
+                + '           			?provenance uc:name ?sourcename '
+                + '                         } '
+                + '                     } '
                 + '                 } '
-                + '             } '
-                + '             GROUP BY ?source '
-                + '         } '
-                + ' } ';
+                + '             }'
+                + '         }group by ?provenance '
+                + ' } '
+                + ' }';
         sparqlQuery.querySrv({query: queryTotalAuthors}, function (rdf) {
             jsonld.compact(rdf, globalData.CONTEXT, function (err, compacted) {
                 //$scope.data = compacted;
@@ -25,24 +37,9 @@ wkhomeControllers.controller('countPublications', ['$scope', 'globalData', 'spar
                 var data = []
                 endpoints.forEach(function (endpoint) {
                     var name = endpoint['uc:name'];
-                    if (name.indexOf('scopus')>-1)
-                    {
-                            data.push({label: 'SCOPUS', value: endpoint['uc:total']['@value']});
-                    }
-                    else
-                    if (name.indexOf('microsoft')>-1)
-                    {
-                            data.push({label: 'MICROSOFT ACADEMICS', value: endpoint['uc:total']['@value']});
-                    }
-                    else
-                    if (name.indexOf('dblp')>-1)
-                    {
-                            data.push({label: 'DBLP', value: endpoint['uc:total']['@value']});
-                    }
-                    else
-                    {
-                        data.push({label: endpoint['uc:name'], value: endpoint['uc:total']['@value']});
-                    }
+                   
+                            data.push({label: name, value: endpoint['uc:total']['@value']});
+                
                 });
                 $scope.$apply(function () {
                     $scope.data = {'entityName': 'Articles', 'data': data};
