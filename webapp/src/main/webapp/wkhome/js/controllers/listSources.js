d6cae4ea160f61fedaa4ced5fefc93de88d285b6
fernandobac03 <fernando_bac03@hotmail.com>
2016-08-22 15:33:05 -0500

    automatic service listed in sources.html of universities registered in the REDI.

---
+++ webapp/src/main/webapp/wkhome/js/controllers/listSources.js
@@ -0,0 +1,61 @@
+/* 
+ * Controller to load/list Authors Sources
+ */
+
+wkhomeControllers.controller('listSources', ['sparqlQuery', 'searchData', '$translate', '$routeParams', '$scope', '$window', 'translateService', 'globalData',
+    function (sparqlQuery, searchData, $translate, $routeParams, $scope, $window, translateService, globalData) {
+
+
+        /*************************************************************/
+        /*query to get the sources in memory */
+        /*************************************************************/
+        // loadAllKeyword();
+        $scope.datasources = [];
+
+        //only keywords that appear in more than 2 articles
+        var queryKeywords = globalData.PREFIX
+                + ' Construct { '
+                + '?subject uc:name ?name. '
+                + '?subject uc:fullName ?fullName. '
+                + '?subject uc:city ?city. '
+                + '?subject uc:province ?province. '
+                + '} '
+                + 'WHERE { '
+                + 'graph <' + globalData.endpointsGraph + '> '
+                + '{ '
+                + '?subject uc:name ?name. '
+                + '?subject uc:fullName ?fullName. '
+                + '?subject uc:city ?city. '
+                + '?subject uc:province ?province. '
+                + '}'
+                + '} ';
+        sparqlQuery.querySrv({query: queryKeywords}, function (rdf) {
+
+            jsonld.compact(rdf, globalData.CONTEXT, function (err, compacted) {
+                _.map(compacted["@graph"], function (pub) {
+                    var model = {};
+                    model["id"] = pub["@id"];
+                    model["name"] = pub["uc:name"];
+                    model["fullName"] = pub["uc:fullName"];
+                    model["city"] = pub["uc:city"];
+                    model["url"] = "http://www." + pub["uc:name"].toLowerCase() + ".edu.ec/";
+                    $scope.datasources.push({name: model["name"], fullName: model["fullName"], url: model["url"], city: model["city"]});
+                });
+                $scope.$apply(function () {
+                    $scope.sources = $scope.datasources;
+                });
+                waitingDialog.hide();
+            });
+        });
+
+        /***********************************/
+        /***********************************/
+
+
+
+
+
+
+
+    }]); //end controller
+
