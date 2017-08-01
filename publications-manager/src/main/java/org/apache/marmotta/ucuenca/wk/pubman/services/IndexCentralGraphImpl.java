35cc86a8e2c8eebc0d845661b99ad5cde4b08a65
Jose Luis <jose.cullcay@ucuenca.edu.ec>
2017-08-01 12:15:25 -0500

    Update in the service for indexing the central graph
    Up until now, the service for indexing publications used only the titles. This update allows the service to index the abstract of the publications.

--- publications-manager/src/main/java/org/apache/marmotta/ucuenca/wk/pubman/services/IndexCentralGraphImpl.java
+++ publications-manager/src/main/java/org/apache/marmotta/ucuenca/wk/pubman/services/IndexCentralGraphImpl.java
@@ -79,59 +79,25 @@ public class IndexCentralGraphImpl implements IndexCentralGraph, Runnable {
             Path p1 = Paths.get("idxCentralGraph");
             FSDirectory index = FSDirectory.open(p1);
 
-            //IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_40, analyzer);
-            //IndexWriter writer = new IndexWriter(dir, config);
-            // 0. Specify the analyzer for tokenizing text.
+            //  Specify the analyzer for tokenizing text.
             //    The same analyzer should be used for indexing and searching
             StandardAnalyzer analyzer = new StandardAnalyzer();
 
-            // 1. create the index
-            //Directory index = new TextFileIndexer("");
+            // Create the index
             IndexWriterConfig config = new IndexWriterConfig(analyzer);
 
             IndexWriter w = new IndexWriter(index, config);
-            /*addDoc(w, "Lucene in Action", "193398817");
-            addDoc(w, "Lucene for Dummies", "55320055Z");
-            addDoc(w, "Managing Gigabytes", "55063554A");
-            addDoc(w, "The Art of Computer Science", "9900333X");*/
-            
             
             List<Map<String, Value>> resultPublications = sparqlService.query(QueryLanguage.SPARQL, queriesService.getPublicationsTitlesQuery());
 
             for (Map<String, Value> pubresource : resultPublications) {
                 String publicationResource = pubresource.get("pub").stringValue();
                 String publicationTitle = StringEscapeUtils.unescapeJava(pubresource.get("title").stringValue());
-                addDoc(w, publicationTitle, publicationResource);
+                String publicationAbstract = StringEscapeUtils.unescapeJava(pubresource.get("abstract").stringValue());
+                addDoc(w, publicationTitle + " " + publicationAbstract, publicationResource);
             }
             
             w.close();
-            
-            // 2. query
-            String querystr = "semantic";
-
-            // the "title" arg specifies the default field to use
-            // when no field is explicitly specified in the query.
-            Query q = new QueryParser("title", analyzer).parse(querystr);
-
-            // 3. search
-            int hitsPerPage = 10;
-            IndexReader reader = DirectoryReader.open(index);
-            IndexSearcher searcher = new IndexSearcher(reader);
-            TopDocs docs = searcher.search(q, hitsPerPage);
-            ScoreDoc[] hits = docs.scoreDocs;
-
-            // 4. display results
-            //System.out.println("Found " + hits.length + " hits.");
-            for (int i = 0; i < hits.length; ++i) {
-                int docId = hits[i].doc;
-                Document d = searcher.doc(docId);
-                System.out.println((i + 1) + ". " + d.get("id") + "\t" + d.get("title"));
-            }
-
-            // reader can only be closed when there
-            // is no need to access the documents any more.
-            reader.close();
-            
             
             return "La indexacion de las publicaciones se han cargado exitosamente.";
         } catch (InvalidArgumentException ex) {
@@ -140,10 +106,10 @@ public class IndexCentralGraphImpl implements IndexCentralGraph, Runnable {
             return "error:  " + ex;
         } catch (IOException ex) {
             return "error:  " + ex;
-        } catch (ParseException ex) {
+        } /*catch (ParseException ex) {
             java.util.logging.Logger.getLogger(IndexCentralGraphImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return "";
+        }*/
+        //return "";
     }
 
     @Override
