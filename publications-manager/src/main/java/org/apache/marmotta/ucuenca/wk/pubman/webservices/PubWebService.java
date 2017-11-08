403743995efc7ec99b1a9e3303d3b76c169aae1f
Xavier Sumba <c.uent@hotmail.com>
2017-11-08 17:22:22 -0500

    add orgs to academics

--- publications-manager/src/main/java/org/apache/marmotta/ucuenca/wk/pubman/webservices/PubWebService.java
+++ publications-manager/src/main/java/org/apache/marmotta/ucuenca/wk/pubman/webservices/PubWebService.java
@@ -109,9 +109,10 @@ public class PubWebService {
     @POST
     @Path(GET_PUBLICATIONS_AK)
     public Response readPublicationsPostAK(@QueryParam("Endpoint") String resultType) {
+        String[] organizations = {"http://redi.cedia.edu.ec/resource/organization/UCUENCA"};
         String params = resultType;
         log.debug("Publications Task", params);
-        String result = commonService.GetDataFromProvidersServiceAcademicsKnowledge();
+        String result = commonService.GetDataFromProvidersServiceAcademicsKnowledge(organizations);
         return Response.ok().entity(result).build();
     }
 
