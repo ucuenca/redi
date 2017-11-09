224b681e56165442a2b2157af663954a74f6fa62
Xavier Sumba <c.uent@hotmail.com>
2017-11-09 11:30:35 -0500

    build request url for scopus provider

--- publications-manager/src/main/java/org/apache/marmotta/ucuenca/wk/pubman/services/ScopusProviderService.java
+++ publications-manager/src/main/java/org/apache/marmotta/ucuenca/wk/pubman/services/ScopusProviderService.java
@@ -5,9 +5,10 @@
  */
 package org.apache.marmotta.ucuenca.wk.pubman.services;
 
-import java.util.ArrayList;
+import edu.emory.mathcs.backport.java.util.Collections;
 import java.util.List;
 import javax.inject.Inject;
+import org.apache.marmotta.platform.core.api.config.ConfigurationService;
 import org.apache.marmotta.ucuenca.wk.commons.service.ConstantService;
 
 /**
@@ -18,29 +19,23 @@ public class ScopusProviderService extends AbstractProviderService {
 
     @Inject
     private ConstantService constantService;
+    @Inject
+    private ConfigurationService configurationService;
 
-    private final String URLSEARCHSCOPUS = "http://api.elsevier.com/content/search/author?query=authfirst%28FIRSTNAME%29authlast%28LASTNAME%29affil%28PAIS%29&apiKey=a3b64e9d82a8f7b14967b9b9ce8d513d&httpAccept=application/xml";
+    private final String requestTemplate = "http://api.elsevier.com/content/search/author?query=%s&count=100&apiKey=%s";
+    private final String expressionTemplate = "authfirst(%s) OR authfirst(%s) AND authlast(%s)";
 
     @Override
+
     protected List<String> buildURLs(String firstname, String lastname) {
+        String apikey = configurationService.getStringConfiguration("publications.scopus.apikey", "");
 
-        //    private List<String> buildURLs(String firstname, String lastname) {
-        //firstNameSearch = firstName.split(" ").length > 1 ? firstName.split(" ")[0] : firstName;
-        //String secondNameSearch = firstName.split(" ").length > 1 ? firstName.split(" ")[1] : "";
-        //String lastNameSearch2 = lastName.split(" ").length > 1 ? lastName.split(" ")[1] : "";
-        //String surnamesSearch = lastNameSearch.split(" ").length > 1 ? firstNameSearch + "+OR+" + secondNameSearch: firstName;
-        //uri_search.add(URLSEARCHSCOPUS.replace("FIRSTNAME", firstNameSearch.replace(" ", "+OR+")).replace("LASTNAME", lastNameSearch).replace(AFFILIATIONPARAM, ""));//.replace("PAIS", "Ecuador"));
-        int numApellidos = cleanNameAuthor(lastname).split(" ").length;
-        String fullname = "&fullName=" + firstname.replace(" ", "%20") + "%20%20" + lastname.replace(" ", "%20");
-        List<String> uri_search = new ArrayList<>();
-        String lastNameSearch = numApellidos > 1 && numApellidos < 3 ? cleanNameAuthor(lastname).split(" ")[0] : lastname.replace(" ", "+OR+");
-        uri_search.add(URLSEARCHSCOPUS.replace("FIRSTNAME", firstname.replace(" ", "+OR+")).replace("LASTNAME", lastname.replace(" ", "+AND+")).replace("PAIS", "Ecuador") + fullname);// .replace(AFFILIATIONPARAM, "")
-        String URLSearch = URLSEARCHSCOPUS.replace("FIRSTNAME", firstname.replace(" ", "+OR+")).replace("LASTNAME", lastNameSearch).replace("PAIS", "Ecuador");
-        if (!uri_search.contains(URLSearch)) {
-            uri_search.add(URLSearch + fullname);
-        }
+        String[] names = firstname.split(" ").length == 2 ? firstname.split(" ") : new String[]{firstname};
+        String lastNameSearch2 = lastname.split(" ").length > 1 ? lastname.split(" ")[0] : lastname;
 
-        return uri_search;
+        String expression = String.format(expressionTemplate, names[0], names[1], lastNameSearch2);
+        String url = String.format(requestTemplate, expression, apikey);
+        return Collections.singletonList(url);
     }
 
     private String cleanNameAuthor(String value) {
