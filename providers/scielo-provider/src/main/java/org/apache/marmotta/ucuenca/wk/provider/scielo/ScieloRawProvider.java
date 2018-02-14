2950134e59756e3710d0b78459cff06e3fa14350
José Ortiz <jeortizvivar@gmail.com>
2018-02-14 11:24:34 -0500

    Scielo provider mapping

--- providers/scielo-provider/src/main/java/org/apache/marmotta/ucuenca/wk/provider/scielo/ScieloRawProvider.java
+++ providers/scielo-provider/src/main/java/org/apache/marmotta/ucuenca/wk/provider/scielo/ScieloRawProvider.java
@@ -33,7 +33,6 @@ import org.jdom2.input.SAXBuilder;
 import org.jdom2.input.sax.XMLReaders;
 import org.jdom2.xpath.XPathFactory;
 import org.openrdf.model.Model;
-import org.openrdf.model.impl.ValueFactoryImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,7 +42,13 @@ import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import org.apache.marmotta.ldclient.model.ClientConfiguration;
+import org.apache.marmotta.ldclient.model.ClientResponse;
+import org.apache.marmotta.ldclient.services.ldclient.LDClient;
 import org.apache.marmotta.ucuenca.wk.commons.disambiguation.Person;
+import org.apache.marmotta.ucuenca.wk.commons.function.LDClientTools;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.ValueFactoryImpl;
 import org.openrdf.model.vocabulary.OWL;
 
 /**
@@ -56,14 +61,8 @@ public class ScieloRawProvider extends AbstractHttpProvider {
     public static final String NAME = "Scielo Raw Provider";
     public static final String API = "https://search.scielo.org/?q=%s&output=xml";
     public static final String PATTERN = "https://search\\.scielo\\.org/search/.*";
-
     private static Logger log = LoggerFactory.getLogger(ScieloRawProvider.class);
-
-    public static final String MOCKSCIELOONT = "https://search.scielo.org/ont/";
-    public static final String MOCKSCIELOBASE = "https://search.scielo.org/";
-
-    private static final String URICONST = "uri";
-    private static final String JOURNALCONST = "journal";
+    public static final String APIPUB = "https://search.scielo.org/searchpub/%s";
 
     /**
      * Return the name of this data provider. To be used e.g. in the
@@ -112,13 +111,51 @@ public class ScieloRawProvider extends AbstractHttpProvider {
     @Override
     public List<String> parseResponse(String resource, String requestUrl, Model triples, InputStream input, String contentType) throws DataRetrievalException {
         log.debug("Request Successful to {0}", requestUrl);
+        String ff = "&from=%s&count=15";
+        String end = "&count=15";
+        ValueFactory factory = ValueFactoryImpl.getInstance();
         try {
-            final Document doc = new SAXBuilder(XMLReaders.NONVALIDATING).build(input);
-            for (Element element : queryElements(doc, "/response/result/doc")) {
-                map(element, triples, resource, getName(resource));
+            Document doc = new SAXBuilder(XMLReaders.NONVALIDATING).build(input);
+            if (!requestUrl.endsWith(end)) {
+                int max = 0;
+                for (Element element : queryElements(doc, "/response/result")) {
+                    if (element.getAttributeValue("name").equals("response")) {
+                        String value = element.getAttributeValue("numFound");
+                        max = Integer.parseInt(value);
+                    }
+                }
+                List<String> e = new ArrayList<>();
+                for (int i = 1; i <= max; i += 15) {
+                    e.add(requestUrl + String.format(ff, i));
+                }
+                return e;
+            } else {
+                ClientConfiguration conf = new ClientConfiguration();
+                LDClient ldClient = new LDClient(conf);
+                List<String> nameOrg = Lists.newArrayList(getName(resource));
+                Person p1 = new Person();
+                p1.Name = new ArrayList<>();
+                p1.Name.add(nameOrg);
 
+                for (Element element : queryElements(doc, "/response/result/doc")) {
+                    String value = getValue(element, "ur");
+                    String code = URLEncoder.encode(value);
+                    List<String> values = getValues(element, "au");
+                    for (int i = 0; i < values.size(); i++) {
+                        Person p2 = new Person();
+                        p2.Name = new ArrayList<>();
+                        p2.Name.add(Lists.newArrayList(values.get(i)));
+                        if (p1.checkName(p2)) {
+                            ClientResponse retryLDClient = LDClientTools.retryLDClient(ldClient, String.format(APIPUB, value), 2, 60);
+                            Model a = retryLDClient.getData();
+                            triples.addAll(a);
+                            String authorURI = ScieloRawPublicationProvider.SCIELOBASEAUTHOR + code + "_" + i;
+                            triples.add(factory.createURI(authorURI), OWL.ONEOF, factory.createURI(resource));
+                        }
+                    }
+
+                }
             }
-
         } catch (IOException e) {
             throw new DataRetrievalException("I/O error while parsing HTML response", e);
         } catch (JDOMException e) {
@@ -127,109 +164,37 @@ public class ScieloRawProvider extends AbstractHttpProvider {
         return Collections.emptyList();
     }
 
-    private static void map(Element element, Model model, String queryURI, String mainAuthorName) {
-        ValueFactoryImpl instance = ValueFactoryImpl.getInstance();
-
-        String paperID = getValue(element, "id");
-        String mockPaperURI = MOCKSCIELOBASE + "publication/" + paperID;
-        //Autores
-        List<String> authors = getValues(element, "au");
-        for (int i = 0; i < authors.size(); i++) {
-            String mockAuthorURI = MOCKSCIELOBASE + "author/" + paperID + "_" + i;
-            model.add(instance.createURI(mockAuthorURI), instance.createURI(MOCKSCIELOONT + "publications"), instance.createURI(mockPaperURI));
-            model.add(instance.createURI(mockAuthorURI), instance.createURI(MOCKSCIELOONT + "name"), instance.createLiteral(authors.get(i)));
-            if (i == 0) {
-                model.add(instance.createURI(mockPaperURI), instance.createURI(MOCKSCIELOONT + "creator"), instance.createURI(mockAuthorURI));
-            }
-            //Si comparacion nombre
-            Person mockPerson1 = new Person();
-            ArrayList<String> namePerson1 = Lists.newArrayList(authors.get(i));
-            mockPerson1.Name = Lists.newArrayList();
-            mockPerson1.Name.add(namePerson1);
-            Person mockPerson2 = new Person();
-            ArrayList<String> namePerson2 = Lists.newArrayList(mainAuthorName);
-            mockPerson2.Name = Lists.newArrayList();
-            mockPerson2.Name.add(namePerson2);
-
-            if (mockPerson1.checkName(mockPerson2)) {
-                model.add(instance.createURI(mockAuthorURI), OWL.ONEOF, instance.createURI(queryURI));
-            }
-
-            //subject_area  wok_subject_categories
-            mapProperty(element, mockAuthorURI, "subject_area", MOCKSCIELOONT + "interestArea", null, model);
-            mapProperty(element, mockAuthorURI, "wok_subject_categories", MOCKSCIELOONT + "interestArea", null, model);
-
-        }
-        //Titulos
-        mapProperty(element, mockPaperURI, "ti", MOCKSCIELOONT + "title", null, model);
-        mapProperty(element, mockPaperURI, "ti_es", MOCKSCIELOONT + "title", "es", model);
-        mapProperty(element, mockPaperURI, "ti_en", MOCKSCIELOONT + "title", "en", model);
-
-        //Abtract
-        mapProperty(element, mockPaperURI, "ab", MOCKSCIELOONT + "abstract", null, model);
-        mapProperty(element, mockPaperURI, "ab_es", MOCKSCIELOONT + "abstract", "es", model);
-        mapProperty(element, mockPaperURI, "ab_en", MOCKSCIELOONT + "abstract", "en", model);
-
-        //Keyword
-        mapProperty(element, mockPaperURI, "keyword", MOCKSCIELOONT + "keywords", null, model);
-        mapProperty(element, mockPaperURI, "keyword_es", MOCKSCIELOONT + "keywords", "es", model);
-        mapProperty(element, mockPaperURI, "keyword_en", MOCKSCIELOONT + "keywords", "en", model);
-
-        //uri
-        mapProperty(element, mockPaperURI, "fulltext_pdf", MOCKSCIELOONT + URICONST, null, model);
-        mapProperty(element, mockPaperURI, "fulltext_pdf_en", MOCKSCIELOONT + URICONST, null, model);
-        mapProperty(element, mockPaperURI, "fulltext_pdf_es", MOCKSCIELOONT + URICONST, null, model);
-        mapProperty(element, mockPaperURI, "fulltext_html", MOCKSCIELOONT + URICONST, null, model);
-        mapProperty(element, mockPaperURI, "fulltext_html_en", MOCKSCIELOONT + URICONST, null, model);
-        mapProperty(element, mockPaperURI, "fulltext_html_es", MOCKSCIELOONT + URICONST, null, model);
-
-        //pages
-        mapProperty(element, mockPaperURI, "pg", MOCKSCIELOONT + "pages", null, model);
-        mapProperty(element, mockPaperURI, "start_page", MOCKSCIELOONT + "pagesS", null, model);
-        mapProperty(element, mockPaperURI, "end_page", MOCKSCIELOONT + "pagesE", null, model);
-
-        //Issue
-        mapProperty(element, mockPaperURI, "issue", MOCKSCIELOONT + "issue", null, model);
-
-        //Journal
-        mapProperty(element, mockPaperURI, "journal_title", MOCKSCIELOONT + JOURNALCONST, null, model);
-        mapProperty(element, mockPaperURI, "ta", MOCKSCIELOONT + JOURNALCONST, null, model);
-
-        //Journal
-        mapProperty(element, mockPaperURI, "journal_title", MOCKSCIELOONT + JOURNALCONST, null, model);
-        mapProperty(element, mockPaperURI, "ta", MOCKSCIELOONT + JOURNALCONST, null, model);
-
-        //ISSN
-        mapProperty(element, mockPaperURI, "issn", MOCKSCIELOONT + "issn", null, model);
-        //DOI
-        mapProperty(element, mockPaperURI, "doi", MOCKSCIELOONT + "doi", null, model);
-
-        //Date
-        mapProperty(element, mockPaperURI, "da", MOCKSCIELOONT + "date", null, model);
-
+    protected static List<Element> queryElements(Document n, String query) {
+        return XPathFactory.instance().compile(query, new ElementFilter()).evaluate(n);
     }
 
-    private static void mapProperty(Element e, String uri, String field, String propertyOnt, String lang, Model m) {
-        ValueFactoryImpl instance = ValueFactoryImpl.getInstance();
-        List<String> values = getValues(e, field);
-        for (int i = 0; i < values.size(); i++) {
-            m.add(instance.createURI(uri), instance.createURI(propertyOnt), instance.createLiteral(values.get(i), lang));
+    private String getName(String id) {
+        return id.replaceAll("_", " ").replaceAll("-", " ");
+    }
+
+    private String getQuery(String id) {
+        String[] split = id.split("_");
+        String query = "";
+        for (int i = 0; i < split.length; i++) {
+            List<String> ls = Lists.newArrayList(split[i].split("-"));
+            String queryP = "";
+            queryP += "(";
+            for (int j = 0; j < ls.size(); j++) {
+                String n = ls.get(j);
+                String tk = (n.length() <= 2) ? n + "*" : n;
+                queryP += tk.toLowerCase() + (j == ls.size() - 1 ? " " : " OR ");
+            }
+            queryP += ")";
+
+            query += queryP + (i == split.length - 1 ? " " : " AND ");
         }
+        return "au:(" + query + ")";
     }
 
     public static String getValue(Element m, String key) {
         List<String> values = getValues(m, key);
         assert values.size() == 1;
         return values.get(0);
-    }
-
-    private String getName(String uri) {
-        String id = uri.substring(uri.lastIndexOf('/') + 1).replaceAll("_", " ").replaceAll("-", " ");
-        return id.trim();
-    }
-
-    protected static List<Element> queryElements(Document n, String query) {
-        return XPathFactory.instance().compile(query, new ElementFilter()).evaluate(n);
     }
 
     public static List<String> getValues(Element m, String key) {
@@ -250,24 +215,4 @@ public class ScieloRawProvider extends AbstractHttpProvider {
         }
         return v;
     }
-
-    private String getQuery(String id) {
-        String[] split = id.split("_");
-        String query = "";
-        for (int i = 0; i < split.length; i++) {
-            List<String> ls = Lists.newArrayList(split[i].split("-"));
-            String queryP = "";
-            queryP += "(au:(";
-            for (int j = 0; j < ls.size(); j++) {
-                String n = ls.get(j);
-                String tk = (n.length() == 1) ? n + "*" : n;
-                queryP += tk.toLowerCase() + (j == ls.size() - 1 ? " " : " OR ");
-            }
-            queryP += "))";
-
-            query += queryP + (i == split.length - 1 ? " " : " AND ");
-        }
-        return query;
-    }
-
 }
