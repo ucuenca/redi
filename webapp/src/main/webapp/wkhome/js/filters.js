a02fb515cd16ad49e0170a7f3a6ddd0aa5dc317c
Xavier Sumba <c.uent@hotmail.com>
2018-03-16 17:31:10 -0500

    publication filters

--- webapp/src/main/webapp/wkhome/js/filters.js
+++ webapp/src/main/webapp/wkhome/js/filters.js
@@ -1,9 +1,17 @@
 'use strict';
 
-/* Filters */
-
-angular.module('phonecatFilters', []).filter('checkmark', function() {
-  return function(input) {
-    return input ? '\u2713' : '\u2718';
+/* Converts a string to CamelCase representation. If the 'strict' argument is given, converts to lower case first. */
+wkhomeApp.filter('camelCase', function() {
+  return function(input, isStrict) {
+    if (!input || (isStrict && typeof(isStrict) !== 'boolean')) {
+      return;
+    }
+    if (isStrict) {
+      input = input.toLowerCase();
+    }
+    var words = input.split(/[ ,]+/);
+    for (var i = 0, len = words.length; i < len; i++)
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    return words.join(' ');
   };
 });
