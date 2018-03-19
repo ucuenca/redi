1e77e617d18fc7ea7f9666ffebf85608e84d77ce
Xavier Sumba <c.uent@hotmail.com>
2018-03-19 12:54:17 -0500

    hide search box in home

--- webapp/src/main/webapp/wkhome/js/controllers/navbar.js
+++ webapp/src/main/webapp/wkhome/js/controllers/navbar.js
@@ -1,5 +1,9 @@
 wkhomeControllers.controller('navbarController', ["$scope", "$location", function ($scope, $location) {
     $scope.isActive = function (viewLocation) {
           return viewLocation === $location.path();
-      };
+    };
+    $scope.isHome = function() {
+      console.log($location);
+      return $location.path() === '/';
+    }
 }]);
