0ae65e94a659a5a71476153342987f7aab4a1d7a
José Ortiz <jeortizvivar@gmail.com>
2018-05-07 11:31:37 -0500

    fix double abbreviations issue

--- tools/commons/src/main/java/org/apache/marmotta/ucuenca/wk/commons/function/StrNamesUtils.java
+++ tools/commons/src/main/java/org/apache/marmotta/ucuenca/wk/commons/function/StrNamesUtils.java
@@ -18,26 +18,35 @@ public final class StrNamesUtils {
 
     private StrNamesUtils() {
     }
-    
-    
-    
+
     public static String or(String name) {
         return or(name, -1);
     }
-    
-    public static String or(String name, int max) {        
+
+    public static String or(String name, int max) {
         name = StringUtils.stripAccents(name).trim().toLowerCase().replaceAll("\\.|,|;|:|-|\n|\\\\|\\||\"|\'|_|/", " ").trim();
         String s = "";
         String[] tokens = name.split(" ");
         List<String> list = new ArrayList<String>(Arrays.asList(tokens));
         list.removeAll(Arrays.asList("", null));
-        if (max!=-1 && max<=list.size()){
-            list = list.subList(0, max);
-        }
+        List<String> list2 = new ArrayList<String>();
         for (int i = 0; i < list.size(); i++) {
-            s += list.get(i) + (i == list.size() - 1 ? "" : "-");
+            if (max == -1 || max != -1 && list2.size() < max) {
+                if (list.get(i).length() > 2) {
+                    list2.add(list.get(i));
+                }
+            }
+        }
+        if (list2.isEmpty()) {
+            list2.addAll(list);
+        }
+        if (max != -1 && max <= list2.size()) {
+            list2 = list2.subList(0, max);
+        }
+        for (int i = 0; i < list2.size(); i++) {
+            s += list2.get(i) + (i == list2.size() - 1 ? "" : "-");
         }
         return s;
     }
-    
+
 }
