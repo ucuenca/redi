e546fadba921432e0e59c44934db742160f87348
xavier.sumba93 <xavier.sumba93@ucuenca.edu.ec>
2018-05-08 18:14:46 -0500

    fix orcid extraction from scopus

--- providers/scopus-provider/src/test/java/ec/edu/cedia/redi/ldclient/test/scopus/TestScopusProvider.java
+++ providers/scopus-provider/src/test/java/ec/edu/cedia/redi/ldclient/test/scopus/TestScopusProvider.java
@@ -5,9 +5,9 @@
  * licenses this file to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -39,6 +39,19 @@ public class TestScopusProvider extends ProviderTestBase {
     @Test
     public void testAuthorSearchSingleAuthor() throws Exception {
         testResource(String.format(templateURL, "authfirst(victor)authlast(saquicela)", apiKey));
+    }
+
+    /**
+     * Tests the Scopus APIs. It looks for an author and checks if the authors
+     * has the expected ORCID.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAuthorSearchRightOrcid() throws Exception {
+        testResource(
+                String.format(templateURL, "authfirst(juan)authlast(carvallo)affil(ecuador)", apiKey),
+                "scopus-check-orcid-extraction.sparql");
     }
 
     /**
