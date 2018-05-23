3d74fea5ec76b251dcf5db3c4e3234cca6c06acf
xavier.sumba93 <xavier.sumba93@ucuenca.edu.ec>
2018-05-23 18:00:40 -0500

    Use appropiate profile icon

--- webapp/src/main/webapp/wkhome/js/visuals/authorProfile.js
+++ webapp/src/main/webapp/wkhome/js/visuals/authorProfile.js
@@ -1,46 +1,67 @@
-
 //var authorprof = angular.module('authorProfile', []);
 //  d3  Factory
 
 
 
-wkhomeApp.directive('profileTemplate', [ 'globalData','sparqlQuery', '$routeParams' , '$window' ,
-    function ( globalData, sparqlQuery , $routeParams, $window ) {
-                
-                
-           return {
-                restrict: 'E',
-                require: '^ngModel',
-                scope: {
-                ngModel: '='
-                },
-            //  template: 'HOLA : {{ngModel.name}}',
-         templateUrl: 'wkhome/partials/profileTemplate.html',
-    link: function(scope, iElement, iAttrs, ctrl) {
-        console.log("DIrectiva");
-    }
-  };
-        }
-    ]);
+wkhomeApp.directive('profileTemplate', ['globalData', 'sparqlQuery', '$routeParams', '$window',
+  function(globalData, sparqlQuery, $routeParams, $window) {
 
 
-wkhomeApp.directive('coauthorTemplate', [ 'globalData','sparqlQuery', '$routeParams' , '$window' ,
-    function ( globalData, sparqlQuery , $routeParams, $window ) {
-                
-                
-           return {
-                restrict: 'E',
-                require: '^ngModel',
-                scope: {
-                ngModel: '=',
-                clickonRelatedauthor: '&'
-                },
-             // template: ' Hola {{ngModel.authorname}} chao',
-             template: ' <ul> <li class="listCoauthors" ng-repeat="coauthor in ngModel.data" > <a target="blank" ng-click="clickonRelatedauthor({uri: coauthor.id });"> <img src="{{coauthor.img}}" class="img-rounded" alt="Logo Cedia" width="30" height="30">  {{coauthor.authorname}} </a> </li> </ul>  ',
-    link: function(scope, iElement, iAttrs, ctrl) {
-   
-        console.log(scope);
+    return {
+      restrict: 'E',
+      require: '^ngModel',
+      scope: {
+        ngModel: '='
+      },
+      //  template: 'HOLA : {{ngModel.name}}',
+      templateUrl: 'wkhome/partials/profileTemplate.html',
+      link: function(scope, iElement, iAttrs, ctrl) {
+        // console.log("DIrectiva");
+      }
+    };
+  }
+]);
+
+
+wkhomeApp.directive('coauthorTemplate', ['globalData', 'sparqlQuery', '$routeParams', '$window',
+  function(globalData, sparqlQuery, $routeParams, $window) {
+
+
+    return {
+      restrict: 'E',
+      require: '^ngModel',
+      scope: {
+        ngModel: '=',
+        clickonRelatedauthor: '&'
+      },
+      // template: ' Hola {{ngModel.authorname}} chao',
+      template: ' <ul> <li class="listCoauthors" ng-repeat="coauthor in ngModel.data" > <a target="blank" ng-click="clickonRelatedauthor({uri: coauthor.id });"> <img src="{{coauthor.img}}" class="img-rounded" alt="Logo Cedia" width="30" height="30">  {{coauthor.authorname}} </a> </li> </ul>  ',
+      link: function(scope, iElement, iAttrs, ctrl) {
+
+        // console.log(scope);
+      }
+    };
+  }
+]);
+
+wkhomeApp.directive('logoProfile', function(){
+  function classifyURLS(scope, element, attrs, controllers){
+    scope.type = undefined;
+    if (scope.uri.indexOf("academic.microsoft.com") !== -1) {
+      scope.type = "academics";
+    } else if (scope.uri.indexOf("www.scopus.com") !== -1){
+      scope.type = "scopus";
+    } else if (scope.uri.indexOf("scholar.google.com") !== -1){
+      scope.type = "scholar";
     }
+  }
+
+  return {
+    restrict: 'E',
+    scope: {
+      uri: '@'
+    },
+    link: classifyURLS,
+    template: '<a href="{{uri}}" target="_blank"><span><img ng-src="wkhome/images/{{type}}_icon.png" width="17" ng-attr-title="{{type}} profile"/></span></a>'
   };
-        }
-    ]);
+});
