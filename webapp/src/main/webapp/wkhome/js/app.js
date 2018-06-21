44bc40ea938aa00ca27115f62681d1bcc87e2a08
xavier.sumba93 <xavier.sumba93@ucuenca.edu.ec>
2018-06-21 12:43:10 -0500

    change queries of cloud

--- webapp/src/main/webapp/wkhome/js/app.js
+++ webapp/src/main/webapp/wkhome/js/app.js
@@ -33,8 +33,8 @@ wkhomeApp.service('searchData', function () {
 });
 
 wkhomeApp.service('globalData', ['$window', function ($window) {
-    //  var baseURL = "http://redi.cedia.edu.ec/";
      var baseURL = $window.location.origin + $window.location.pathname;
+    //  baseURL = baseURL.replace("http","https").replace(":" + $window.location.port, "");
      this.serverInstance=baseURL;
      baseURL = baseURL.replace("clon","");
      baseURL = baseURL.replace("localhost:8080","redi.cedia.edu.ec");
