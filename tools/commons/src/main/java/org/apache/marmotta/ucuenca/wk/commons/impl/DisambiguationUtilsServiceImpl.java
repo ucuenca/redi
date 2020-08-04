4d173c0612f3c1c30326c10e0a4b25c29b0d3af5
Jose Ortiz <jeortizvivar@gmail.com>
2020-08-04 15:55:27 -0500

    add isGivenName common function

--- tools/commons/src/main/java/org/apache/marmotta/ucuenca/wk/commons/impl/DisambiguationUtilsServiceImpl.java
+++ tools/commons/src/main/java/org/apache/marmotta/ucuenca/wk/commons/impl/DisambiguationUtilsServiceImpl.java
@@ -27,46 +27,77 @@ import org.openrdf.query.QueryLanguage;
  */
 public class DisambiguationUtilsServiceImpl implements DisambiguationUtilsService {
 
-  @Inject
-  private ExternalSPARQLService sparqlService;
-  @Inject
-  private ConstantService con;
+    @Inject
+    private ExternalSPARQLService sparqlService;
+    @Inject
+    private ConstantService con;
 
-  @Override
-  public List<String> lookForOrganizations(List<String> aff) throws MarmottaException {
-    Person mock = new Person();
-    mock.Affiliations = new ArrayList<>();
-    mock.Affiliations.addAll(aff);
-    List<String> uris = Lists.newArrayList();
-    Map<String, Set<String>> organizations = getOrganizations();
-    for (Map.Entry<String, Set<String>> org : organizations.entrySet()) {
-      Person foo = new Person();
-      foo.Affiliations = new ArrayList<>();
-      foo.Affiliations.addAll(org.getValue());
-      if (mock.checkAffiliations(foo)) {
-        uris.add(org.getKey());
-      }
+    @Override
+    public List<String> lookForOrganizations(List<String> aff) throws MarmottaException {
+        Person mock = new Person();
+        mock.Affiliations = new ArrayList<>();
+        mock.Affiliations.addAll(aff);
+        List<String> uris = Lists.newArrayList();
+        Map<String, Set<String>> organizations = getOrganizations();
+        for (Map.Entry<String, Set<String>> org : organizations.entrySet()) {
+            Person foo = new Person();
+            foo.Affiliations = new ArrayList<>();
+            foo.Affiliations.addAll(org.getValue());
+            if (mock.checkAffiliations(foo)) {
+                uris.add(org.getKey());
+            }
+        }
+        return uris;
     }
-    return uris;
-  }
 
-  private Map<String, Set<String>> getOrganizations() throws MarmottaException {
-    ConcurrentHashMap<String, Set<String>> mp = new ConcurrentHashMap<>();
-    String qry = "select * {\n"
-            + "    graph <" + con.getOrganizationsGraph() + "> {\n"
-            + "        ?o <http://ucuenca.edu.ec/ontology#name>|<http://ucuenca.edu.ec/ontology#alias>|<http://ucuenca.edu.ec/ontology#fullName> ?n .\n"
-            + "    }\n"
-            + "}";
-    List<Map<String, Value>> query = sparqlService.getSparqlService().query(QueryLanguage.SPARQL, qry);
-    for (Map<String, Value> m : query) {
-      String uri = m.get("o").stringValue();
-      String[] name = m.get("n").stringValue().split(";");
-      if (!mp.containsKey(uri)) {
-        mp.put(uri, new HashSet<String>());
-      }
-      mp.get(uri).addAll(Lists.newArrayList(name));
+    private Map<String, Set<String>> getOrganizations() throws MarmottaException {
+        ConcurrentHashMap<String, Set<String>> mp = new ConcurrentHashMap<>();
+        String qry = "select * {\n"
+                + "    graph <" + con.getOrganizationsGraph() + "> {\n"
+                + "        ?o <http://ucuenca.edu.ec/ontology#name>|<http://ucuenca.edu.ec/ontology#alias>|<http://ucuenca.edu.ec/ontology#fullName> ?n .\n"
+                + "    }\n"
+                + "}";
+        List<Map<String, Value>> query = sparqlService.getSparqlService().query(QueryLanguage.SPARQL, qry);
+        for (Map<String, Value> m : query) {
+            String uri = m.get("o").stringValue();
+            String[] name = m.get("n").stringValue().split(";");
+            if (!mp.containsKey(uri)) {
+                mp.put(uri, new HashSet<String>());
+            }
+            mp.get(uri).addAll(Lists.newArrayList(name));
+        }
+        return mp;
     }
-    return mp;
-  }
+
+    @Override
+    public double isGivenName(String gn) throws MarmottaException {
+        String q = "PREFIX inst: <http://www.ontotext.com/connectors/lucene/instance#>\n"
+                + "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n"
+                + "PREFIX : <http://www.ontotext.com/connectors/lucene#>\n"
+                + "select ?t where { \n"
+                + "    bind('" + gn + "' as ?n) .\n"
+                + "    bind (replace(replace(lcase(?n),':|,|-|\\\\.|/',' '),'\\\\s+',' ') as ?nn_) .\n"
+                + "    bind (replace(concat(replace(replace(?nn_,' $',''),'^ ',''), ' '),' ',' ') as ?nn) .\n"
+                + "    bind (concat ('givenName:(',?nn, ')') as ?gq).\n"
+                + "    bind (concat ('familyName:(',?nn, ')') as ?fq).\n"
+                + "    ?s1 a inst:namesidx  ;\n"
+                + "    	:query  ?gq;\n"
+                + "        :totalHits ?tg .             \n"
+                + "    ?s2 a inst:namesidx  ;\n"
+                + "    	:query  ?fq;\n"
+                + "        :totalHits ?tf .         \n"
+                + "    bind ( ?tg/(?tf+?tg) as ?t ) . \n"
+                + "}";
+
+        List<Map<String, Value>> query = sparqlService.getSparqlService().query(QueryLanguage.SPARQL, q);
+        double r = Double.NaN;
+        for (Map<String, Value> mp : query) {
+            Value get = mp.get("t");
+            if (get != null) {
+                r = Double.parseDouble(get.stringValue());
+            }
+        }
+        return r;
+    }
 
 }
