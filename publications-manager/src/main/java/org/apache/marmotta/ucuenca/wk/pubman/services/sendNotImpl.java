972ac85bc95ee8ec75d181d878c7260bd91eef22
Jose Ortiz <jeortizvivar@gmail.com>
2020-11-23 18:41:34 -0500

    Send mail notifications beta

--- publications-manager/src/main/java/org/apache/marmotta/ucuenca/wk/pubman/services/sendNotImpl.java
+++ publications-manager/src/main/java/org/apache/marmotta/ucuenca/wk/pubman/services/sendNotImpl.java
@@ -7,6 +7,15 @@ package org.apache.marmotta.ucuenca.wk.pubman.services;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import freemarker.core.ParseException;
+import freemarker.template.Configuration;
+import freemarker.template.MalformedTemplateNameException;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -18,8 +27,14 @@ import org.apache.marmotta.platform.core.exception.MarmottaException;
 import org.apache.marmotta.ucuenca.wk.commons.service.ExternalSPARQLService;
 import org.slf4j.Logger;
 import org.apache.marmotta.ucuenca.wk.pubman.api.SendNotificationsMarmotta;
+import org.apache.marmotta.ucuenca.wk.pubman.utils.freeMarkerModel;
 import org.openrdf.model.Value;
 import org.openrdf.query.QueryLanguage;
+import org.simplejavamail.email.Email;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.email.EmailPopulatingBuilder;
+import org.simplejavamail.mailer.Mailer;
+import org.simplejavamail.mailer.MailerBuilder;
 
 /**
  *
@@ -67,29 +82,65 @@ public class sendNotImpl implements SendNotificationsMarmotta {
         if (!mp.containsKey(stringValue)) {
           mp.put(stringValue, new Object[]{(String) null, new String[]{}, new ArrayList<String>(), new ArrayList<String>()});
         }
-        mp.get(stringValue)[0] = mm.get("an") .stringValue() ;//0: Name
+        mp.get(stringValue)[0] = mm.get("an").stringValue();//0: Name
         mp.get(stringValue)[1] = mm.get("ae") != null ? mm.get("ae").stringValue().toLowerCase().split(";;;") : mp.get(stringValue)[1];//1: Email
         ((List<String>) mp.get(stringValue)[2]).add(mm.get("pu").stringValue());
         ((List<String>) mp.get(stringValue)[3]).add(mm.get("pt").stringValue());
       }
       nofifyNewPublications(mp);
 
-    } catch (MarmottaException ex) {
+      //TODO: delete notifications graph
+    } catch (Exception ex) {
       ex.printStackTrace();
     }
   }
 
-  private void nofifyNewPublications(Map<String, Object[]> mp) {
+  private void nofifyNewPublications(Map<String, Object[]> mp) throws MalformedTemplateNameException, ParseException, IOException, TemplateException {
+    Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+    cfg.setClassForTemplateLoading(this.getClass(), "/");
+    cfg.setDefaultEncoding("UTF-8");
+    cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+    cfg.setLogTemplateExceptions(false);
+    cfg.setWrapUncheckedExceptions(true);
+    cfg.setFallbackOnNullLoopVariable(false);
+    Template temp = cfg.getTemplate("freemaker/notification.ftlh");
+
+    int i = 0;
+
     for (Map.Entry<String, Object[]> au : mp.entrySet()) {
+      i++;
       String authorURI = au.getKey();
       String authorName = ((String) au.getValue()[0]);
       String[] authorEmails = ((String[]) au.getValue()[1]);
       List<String> newPublicationsURI = ((List<String>) au.getValue()[2]);
-      List<String> newPublicationsTitle = ((List<String>) au.getValue()[2]);
+      List<String> newPublicationsTitle = ((List<String>) au.getValue()[3]);
+
+      freeMarkerModel.notification newNotification = new freeMarkerModel().newNotification(authorName, authorURI, newPublicationsURI, newPublicationsTitle);
+      StringWriter stringWriter = new StringWriter();
+      temp.process(newNotification, stringWriter);
+      stringWriter.flush();
+      stringWriter.close();
+      String toString = stringWriter.toString();
+
+      if (i < 5) {
+        //STMP Temporal solo para pruebas
+        Mailer mailer = MailerBuilder
+                .withSMTPServer("mail.delparamo.net", 8025, "tmp23b0f1a941@delparamo.net", "ih@Npsz_Osy1")
+                .clearEmailAddressCriteria() // turns off email validation
+                .withDebugLogging(true)
+                .buildMailer();
+
+        //para probar el envio abrir: https://generator.email/yaymen.sediek@azel.xyz
+        Email buildEmail = EmailBuilder.startingBlank()
+                .from("tmp23b0f1a941@delparamo.net")
+                .to("yaymen.sediek@azel.xyz")
+                .withSubject("Notificacion de REDI")
+                .withHTMLText(toString).buildEmail();
+
+        mailer.sendMail(buildEmail, false);
+      }
 
       //send notifications
-      
-      
     }
   }
 
---
+++ publications-manager/src/main/java/org/apache/marmotta/ucuenca/wk/pubman/utils/freeMarkerModel.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.apache.marmotta.ucuenca.wk.pubman.utils;
+
+import com.beust.jcommander.internal.Lists;
+import java.util.List;
+
+/**
+ *
+ * @author cedia
+ */
+public class freeMarkerModel {
+
+  public notification newNotification(String name, String URI, List<String> newPublicationsURI, List<String> newPublicationsTitle) {
+    return new notification(name, URI, newPublicationsURI, newPublicationsTitle);
+  }
+
+  public class notification {
+
+    public notification(String name, String URI, List<String> newPublicationsURI, List<String> newPublicationsTitle) {
+      this.name = name;
+      this.URI = URI;
+      this.lsPapers = Lists.newArrayList();
+      for (int i = 0; i < newPublicationsTitle.size(); i++) {
+        this.lsPapers.add(new paper(newPublicationsTitle.get(i), newPublicationsURI.get(i)));
+      }
+    }
+
+    private String name;
+    private String URI;
+    private List<paper> lsPapers;
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public String getURI() {
+      return URI;
+    }
+
+    public void setURI(String URI) {
+      this.URI = URI;
+    }
+
+    public List<paper> getLsPapers() {
+      return lsPapers;
+    }
+
+    public void setLsPapers(List<paper> lsPapers) {
+      this.lsPapers = lsPapers;
+    }
+
+  }
+
+  public class paper {
+
+    public paper(String title, String URL) {
+      this.title = title;
+      this.URL = URL;
+    }
+
+    private String title;
+    private String URL;
+
+    public String getTitle() {
+      return title;
+    }
+
+    public void setTitle(String title) {
+      this.title = title;
+    }
+
+    public String getURL() {
+      return URL;
+    }
+
+    public void setURL(String URL) {
+      this.URL = URL;
+    }
+
+  }
+
+}
