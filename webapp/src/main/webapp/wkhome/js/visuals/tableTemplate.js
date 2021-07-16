791ac0ddc31d82780d25dc0a256f7118d1f70695
Jose Ortiz <jeortizvivar@gmail.com>
2021-07-15 19:23:56 -0500

    Add tool-tips in author metrics section and customize column names

--- webapp/src/main/webapp/wkhome/js/visuals/tableTemplate.js
+++ webapp/src/main/webapp/wkhome/js/visuals/tableTemplate.js
@@ -6,12 +6,21 @@ wkhomeApp.directive('tableTemplate', ['globalData', 'sparqlQuery', '$routeParams
     return {
       restrict: 'E',
       scope: {
-        data: '='
+        data: '=',
+        columnstr: '@',
+        columns: '=?'
+      },
+      controller: function($scope){
+        // check if it was defined.  If not - set a default
+        if (angular.isDefined($scope.columnstr)) {
+            $scope.columns = $scope.columnstr.split(",");
+        }
+        $scope.columns = angular.isDefined($scope.columns) ? $scope.columns : ['Name', 'Value'];
       },
       // template: 'HOLA : {{data}}',
       templateUrl: 'wkhome/partials/table.html',
       link: function(scope, iElement, iAttrs, ctrl) {
-      /*console.log ("DATOS");
+        /*console.log ("DATOS");
       console.log (scope);
       console.log (data);
          $('#example').DataTable( {
